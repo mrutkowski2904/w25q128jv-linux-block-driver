1f1f93a39e8be3cec5a461be48cf6f107faf0109
Maciej Rutkowski <maciejr2904@gmail.com>
2023-05-26 19:20:45 +0200

    registering device as disk

--- w25q128jv.c
+++ w25q128jv.c
@@ -1,8 +1,17 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/of.h>
+#include <linux/fs.h>
+#include <linux/blkdev.h>
+#include <linux/blk-mq.h>
 
 #include "w25q128jv.h"
+
+static int w25q128jv_open(struct block_device *dev, fmode_t mode);
+static void w25q128jv_release(struct gendisk *gdisk, fmode_t mode);
+int w25q128jv_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd, unsigned long arg);
+
+static blk_status_t queue_request(struct blk_mq_hw_ctx *ctx, const struct blk_mq_queue_data *data);
 
 static int w25q128jv_probe(struct spi_device *client);
 static void w25q128jv_remove(struct spi_device *client);
@@ -30,6 +39,37 @@ static struct spi_driver w25q128jv_spi_driver = {
     },
 };
 
+static struct block_device_operations block_dev_ops = {
+    .owner = THIS_MODULE,
+    .open = w25q128jv_open,
+    .release = w25q128jv_release,
+};
+
+static struct blk_mq_ops block_mq_ops = {
+    .queue_rq = queue_request,
+};
+
+struct driver_data drv_data;
+
+static int w25q128jv_open(struct block_device *dev, fmode_t mode)
+{
+    return 0;
+}
+
+static void w25q128jv_release(struct gendisk *gdisk, fmode_t mode)
+{
+}
+
+int w25q128jv_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd, unsigned long arg)
+{
+    return -ENOTTY;
+}
+
+static blk_status_t queue_request(struct blk_mq_hw_ctx *ctx, const struct blk_mq_queue_data *data)
+{
+    return BLK_STS_OK;
+}
+
 static int w25q128jv_probe(struct spi_device *client)
 {
     int status;
@@ -42,16 +82,79 @@ static int w25q128jv_probe(struct spi_device *client)
     spi_set_drvdata(client, dev_data);
     dev_data->client = client;
 
-    dev_info(&client->dev, "probe called");
+    dev_data->tag_set.ops = &block_mq_ops;
+	dev_data->tag_set.nr_hw_queues = 1;
+	dev_data->tag_set.nr_maps = 1;
+	dev_data->tag_set.queue_depth = 16;
+	dev_data->tag_set.numa_node = NUMA_NO_NODE;
+	dev_data->tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
+    status = blk_mq_alloc_tag_set(&dev_data->tag_set);
+	if (status)
+        return status;
+
+    dev_data->disk = blk_mq_alloc_disk(&dev_data->tag_set, NULL);
+    if (IS_ERR(dev_data->disk))
+    {
+        blk_mq_free_tag_set(&dev_data->tag_set);
+		return PTR_ERR(dev_data->disk);
+    }
+
+    dev_data->disk->flags |= GENHD_FL_NO_PART;
+    dev_data->disk->major = drv_data.major;
+    dev_data->disk->first_minor = drv_data.device_no;
+    dev_data->disk->minors = 1;
+    dev_data->disk->fops = &block_dev_ops;
+    dev_data->disk->private_data = dev_data;
+    sprintf(dev_data->disk->disk_name, W25Q128JV_DISK_NAME);
+    set_capacity(dev_data->disk, W25Q128JV_CAPACITY);
+
+    status = add_disk(dev_data->disk);
+    if(status)
+    {
+        blk_mq_free_tag_set(&dev_data->tag_set);
+        put_disk(dev_data->disk);
+        return status;
+    }
+
+    drv_data.device_no++;
+    dev_info(&client->dev, "device inserted");
     return 0;
 }
 
 static void w25q128jv_remove(struct spi_device *client)
 {
-    dev_info(&client->dev, "remove called");
+    struct device_data *dev_data;
+	dev_data = spi_get_drvdata(client);
+
+    if(dev_data->disk)
+    {
+        del_gendisk(dev_data->disk);
+        blk_mq_free_tag_set(&dev_data->tag_set);
+    }
+    dev_info(&client->dev, "device removed");
 }
 
-module_spi_driver(w25q128jv_spi_driver);
+static int __init w25q128jv_driver_init(void)
+{
+    int status;
+    drv_data.device_no = 0;
+    drv_data.major = register_blkdev(0, W25Q128JV_DISK_NAME);
+
+    status = spi_register_driver(&w25q128jv_spi_driver);
+    if (status < 0)
+        return status;
+
+    return status;
+}
+
+static void __exit w25q128jv_driver_exit(void)
+{
+    spi_unregister_driver(&w25q128jv_spi_driver);
+    unregister_blkdev(drv_data.major, W25Q128JV_DISK_NAME);
+}
+
+module_init(w25q128jv_driver_init);
+module_exit(w25q128jv_driver_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Maciej Rutkowski");
--- w25q128jv.h
+++ w25q128jv.h
@@ -3,10 +3,28 @@
 
 #include <linux/types.h>
 #include <linux/spi/spi.h>
+#include <linux/blkdev.h>
+#include <linux/blk-mq.h>
+
+#define W25Q128JV_DISK_NAME "w25q128jv"
+#define W25Q128JV_SECTOR_SIZE 512
+#define W25Q128JV_SECTORS 32768
+/* 16 MiB */
+#define W25Q128JV_CAPACITY (W25Q128JV_SECTOR_SIZE * W25Q128JV_SECTORS)
 
 struct device_data
 {
     struct spi_device *client;
+
+    struct gendisk *disk;
+    struct blk_mq_tag_set tag_set;
+    struct request_queue *req_queue;
+};
+
+struct driver_data
+{
+    int major;
+    int device_no;
 };
 
 #endif /* _W25Q128JV_H */
